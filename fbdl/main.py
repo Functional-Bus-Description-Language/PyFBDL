46882ec020344b0ee9e2268d780c47f6cf986185
Michał Kruszewski <mkru@protonmail.com>
2021-08-08 08:18:08 +0200

    Add command line arguments.

--- fbdl/main.py
+++ fbdl/main.py
@@ -1,12 +1,31 @@
+import argparse
 import logging as log
 import sys
 
 VERSION = "0.0.0"
 
 
+def parse_cmd_line_args():
+    parser = argparse.ArgumentParser(
+        prog="fbdl",
+        description="Functional Bus Description Language compiler front-end written in Python."
+    )
+
+    parser.add_argument("main", help="Path to the main file.")
+
+    parser.add_argument(
+        "-l", help="Log level. The default is 'info'.", choices=["debug", "info", "warn", "error"], default="info"
+    )
+
+    return parser.parse_args()
+
+
 def main():
+    cmd_line_args = parse_cmd_line_args()
+
+    log_levels = {'debug' : log.DEBUG, 'info' : log.INFO, 'warn' : log.WARN, 'error' : log.ERROR}
     log.basicConfig(
-        level=log.INFO, format="%(levelname)s: %(message)s", stream=sys.stderr
+        level=log_levels[cmd_line_args.l], format="%(levelname)s: %(message)s", stream=sys.stderr
     )
     log.info("Hello from fbdl!")
 
