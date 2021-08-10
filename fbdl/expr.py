2160fc38f30792422872ee454cedb307d86cf1f2
Michał Kruszewski <mkru@protonmail.com>
2021-08-10 06:25:49 +0200

    Add identifier evaluation.

--- fbdl/expr.py
+++ fbdl/expr.py
@@ -6,16 +6,18 @@ This is a try to evluate the value during parsing.
 This makes the post parsing expression evaluations shorter.
 """
 import sys
+from pprint import pprint
 
 this_module = sys.modules[__name__]
 
 
 class ExprDict(dict):
-    def __init__(self, cursor, node):
+    def __init__(self, parser, node):
         super().__init__()
+        self.parser = parser
         self._value = None
 
-        self['String'] = cursor.get_node_string(node)
+        self['String'] = parser.get_node_string(node)
         self['Kind'] = node.type
 
     @property
@@ -56,63 +58,67 @@ class ExprDict(dict):
             return  left >> right
 
     def evaluate_identifier(self):
-        pass
+        pkg_symbols = self.parser.this_pkg['Symbols']
+        if not self['String'] in pkg_symbols:
+            raise KeyError(f"Can't find symbol '{self['String']}' in package '{self.parser.this_pkg['Path']}'.")
+
+        return pkg_symbols[self['String']]['Value'].value
 
 
-
-def build_binary_operation(cursor, node):
-    bo = ExprDict(cursor, node)
+def build_binary_operation(parser, node):
+    bo = ExprDict(parser, node)
 
     left_child = node.children[0]
     right_child = node.children[2]
 
-    bo['Left'] = getattr(this_module, 'build_' + left_child.type)(cursor, left_child)
-    bo['Operator'] = cursor.get_node_string(node.children[1])
-    bo['Right'] = getattr(this_module, 'build_' + right_child.type)(cursor, right_child)
+    bo['Left'] = getattr(this_module, 'build_' + left_child.type)(parser, left_child)
+    bo['Operator'] = parser.get_node_string(node.children[1])
+    bo['Right'] = getattr(this_module, 'build_' + right_child.type)(parser, right_child)
 
     bo.value
 
     return bo
 
 
-def build_decimal_literal(cursor, node):
-    dl = ExprDict(cursor, node)
+def build_decimal_literal(parser, node):
+    dl = ExprDict(parser, node)
     dl.value = int(dl['String'])
 
     return dl
 
 
-def build_expression(cursor, node):
-    e = ExprDict(cursor, node)
+def build_expression(parser, node):
+    e = ExprDict(parser, node)
 
     child_node = node.children[0]
-    e['Child'] = getattr(this_module, 'build_' + child_node.type)(cursor, child_node)
+    e['Child'] = getattr(this_module, 'build_' + child_node.type)(parser, child_node)
 
     e.value
 
     return e
 
 
-def build_identifier(cursor, node):
-    i = ExprDict(cursor, node)
+def build_identifier(parser, node):
+    i = ExprDict(parser, node)
 
     i.value
 
     return i
 
-def build_primary_expression(cursor, node):
-    pe = ExprDict(cursor, node)
+
+def build_primary_expression(parser, node):
+    pe = ExprDict(parser, node)
 
     child_node = node.children[0]
-    pe['Child'] = getattr(this_module, 'build_' + child_node.type)(cursor, child_node)
+    pe['Child'] = getattr(this_module, 'build_' + child_node.type)(parser, child_node)
 
     pe.value
 
     return pe
 
 
-def build_true(cursor, node):
-    t = ExprDict(cursor, node)
+def build_true(parser, node):
+    t = ExprDict(parser, node)
     t.value = True
 
     return t
--- fbdl/ts.py
+++ fbdl/ts.py
@@ -11,23 +11,45 @@ Language.build_library('build/fbdl.so', ['submodules/tree-sitter-fbdl/'])
 
 FBDLANG = Language('build/fbdl.so', 'fbdl')
 
-parser = Parser()
-parser.set_language(FBDLANG)
+ts_parser = Parser()
+ts_parser.set_language(FBDLANG)
 
 import expr
 
-class Cursor:
-    def __init__(self, tree, code, file_path):
+
+class Parser():
+    def __init__(self, tree, code, this_file, this_pkg, packages):
         self.tree = tree
-        self.tree_cursor = tree.walk()
+        self.cursor = tree.walk()
         self.code = code
-        self.file_path = file_path
+        self.this_file = this_file
+        self.this_pkg = this_pkg
+        self.packages = packages
 
     def __getattr__(self, name):
-        return self.tree_cursor.__getattribute__(name)
+        return self.cursor.__getattribute__(name)
 
     def get_node_string(self, node):
         return self.code[node.start_byte : node.end_byte].decode('utf8')
+
+    def check_for_errors(self):
+        msg = ""
+        for node in traverse_tree(self.tree):
+            if node.type == 'ERROR':
+                msg += (
+                    "\n  Line "
+                    + str(node.start_point[0] + 1)
+                    + ", character "
+                    + str(node.start_point[1] + 1)
+                )
+
+        if msg:
+            raise Exception(
+                f"Found errors in file '{self.file_path}':"
+                + msg
+                + "\nBe careful, error location returned by the tree-sitter might be misleading."
+                + "\nEspecially if more than one error is reported."
+            )
 
 
 def traverse_tree(tree):
@@ -53,103 +75,92 @@ def traverse_tree(tree):
                 retracing = False
 
 
-def check_for_errors(cursor):
-    msg = ""
-    for node in traverse_tree(cursor.tree):
-        if node.type == 'ERROR':
-            msg += (
-                "\n  Line "
-                + str(node.start_point[0] + 1)
-                + ", character "
-                + str(node.start_point[1] + 1)
-            )
-
-    if msg:
-        raise Exception(
-            f"Found errors in file '{cursor.file_path}':"
-            + msg
-            + "\nBe careful, error location returned by the tree-sitter might be misleading."
-            + "\nEspecially if more than one error is reported."
-        )
-
-
 def parse(packages):
     for package, list_ in packages.items():
         for pkg in list_:
             for f in pkg['Files']:
-                f['Symbols'] = parse_file(f['Handle'])
+                parse_file(f, pkg, packages)
 
 
-def parse_file(fh):
+def parse_file(this_file, this_pkg, packages):
     """
     Parameters:
+    this_file
+    this_pkg
+    packages
     -----------
-    fh
-        File handle.
     """
-    fh.seek(0)
+    if 'Symbols' not in this_pkg:
+        this_pkg['Symbols'] = {}
 
-    symbols = {}
-    code = bytes(fh.read(), 'utf8')
+    this_file['Symbols'] = {}
 
-    tree = parser.parse(code)
-    cursor = Cursor(tree, code, fh.name)
-    check_for_errors(cursor)
+    this_file['Handle'].seek(0)
+    code = bytes(this_file['Handle'].read(), 'utf8')
 
-    if cursor.goto_first_child() == False:
+    tree = ts_parser.parse(code)
+    parser = Parser(tree, code, this_file['Path'], this_pkg, packages)
+    parser.check_for_errors()
+
+    if parser.goto_first_child() == False:
         return symbols
 
     while True:
-        node_type = cursor.node.type
+        node_type = parser.node.type
         # Imports have to be handled in different way, as they are not classical symbols.
         if node_type == 'single_import_statement':
-            parse_single_import_statement(cursor)
+            parse_single_import_statement(parser)
         elif node_type == 'ERROR':
             raise Exception(f"ERROR parsing file '{fh.name}'")
         else:
-            for name, symbol in getattr(this_module, 'parse_' + node_type)(cursor):
-                if name and name in symbols:
+            for name, symbol in getattr(this_module, 'parse_' + node_type)(parser):
+                if name and name in this_file['Symbols']:
                     raise Exception(
-                        f"Symbol '{name}' defined at least twice in file 'fh.name'."
+                        f"Symbol '{name}' defined at least twice in file '{this_file['Path']}'."
                     )
                 elif name:
-                    symbols[name] = symbol
+                    this_file['Symbols'][name] = symbol
 
-        if not cursor.goto_next_sibling():
+                if name and name in this_pkg['Symbols']:
+                    raise Exception(
+                        f"Symbol '{name}' defined at least twice in file '{this_file['Path']}'."
+                    )
+                elif name:
+                    this_pkg['Symbols'][name] = symbol
+
+        if not parser.goto_next_sibling():
             break
 
-    return symbols
 
-
-def parse_element_anonymous_instantiation(cursor):
+def parse_element_anonymous_instantiation(parser):
     symbol = {'Kind': 'Element'}
     return [(None, None)]
 
 
-def parse_multi_constant_definition(cursor):
+def parse_multi_constant_definition(parser):
     symbols = []
 
-    for i in range(len(cursor.node.children) // 3):
+    for i in range(len(parser.node.children) // 3):
         symbol = {'Kind': 'Constant'}
-        name = cursor.get_node_string(cursor.node.children[i * 3 + 1])
+        name = parser.get_node_string(parser.node.children[i * 3 + 1])
 
-        expression = {'String': cursor.get_node_string(cursor.node.children[i * 3 + 3])}
-        symbol['Expression'] = expression
+        expression = {'String': parser.get_node_string(parser.node.children[i * 3 + 3])}
+        symbol['Value'] = expression
 
         symbols.append((name, symbol))
 
     return symbols
 
 
-def parse_single_constant_definition(cursor):
+def parse_single_constant_definition(parser):
     symbol = {'Kind': 'Constant'}
-    name = cursor.get_node_string(cursor.node.children[1])
+    name = parser.get_node_string(parser.node.children[1])
 
-    expression = expr.build_expression(cursor, cursor.node.children[3])
-    symbol['Expression'] = expression
+    expression = expr.build_expression(parser, parser.node.children[3])
+    symbol['Value'] = expression
 
     return [(name, symbol)]
 
 
-def parse_single_import_statement(cursor):
+def parse_single_import_statement(parser):
     return [(None, None)]
