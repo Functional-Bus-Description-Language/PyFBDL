e7468ad165d369b934bf6d6ade1ab13917b04061
Michał Kruszewski <mkru@protonmail.com>
2021-08-25 16:58:26 +0200

    Add parsing tests.

--- fbdl/ts.py
+++ fbdl/ts.py
@@ -1,6 +1,9 @@
 """
 Module for code utilizing tree-sitter.
 """
+import os
+dirname = os.path.dirname(os.path.abspath(__file__))
+
 from pprint import pformat
 import sys
 
@@ -8,9 +11,8 @@ this_module = sys.modules[__name__]
 
 from tree_sitter import Language, Parser, TreeCursor
 
-Language.build_library('build/fbdl.so', ['submodules/tree-sitter-fbdl/'])
-
-FBDLANG = Language('build/fbdl.so', 'fbdl')
+Language.build_library(dirname + '/../build/fbdl.so', [dirname + '/../submodules/tree-sitter-fbdl/'])
+FBDLANG = Language(dirname + '/../build/fbdl.so', 'fbdl')
 
 ts_parser = Parser()
 ts_parser.set_language(FBDLANG)
---
+++ scripts/test_parsing.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Script for running parsing tests.
+# Must be run from projects root.
+
+set -e
+
+export PYTHONPATH=$PWD
+
+cd tests/parsing/
+
+for dir in $(find . -maxdepth 1 -mindepth 1 -type d);
+do
+	echo "********************************************************************************"
+	echo "Running test $dir"
+	echo "********************************************************************************"
+	cd $dir
+	../../../main.py -d -p packages.tmp bus.fbd
+	sed '/Path/d' packages.tmp > packages.tmp.sed
+	sed '/Path/d' packages.golden > packages.golden.sed
+	cmp packages.golden.sed packages.tmp.sed
+	rm packages.tmp
+	rm *.sed
+	cd ..
+	echo
+	done
