7b64bc1239a887fe037a3b781a8b75f62bc4250b
Michał Kruszewski <mkru@protonmail.com>
2021-08-26 09:21:02 +0200

    Divide parsing tests into valid and invalid constructs.

--- scripts/test_parsing.sh
+++ scripts/test_parsing.sh
@@ -9,18 +9,20 @@ export PYTHONPATH=$PWD
 
 cd tests/parsing/
 
-for dir in $(find . -maxdepth 1 -mindepth 1 -type d);
+echo -e "Testing valid constructs\n"
+
+for dir in $(find valid/ -maxdepth 1 -mindepth 1 -type d);
 do
 	echo "********************************************************************************"
 	echo "Running test $dir"
 	echo "********************************************************************************"
 	cd $dir
-	python3 ../../../main.py -d -p packages.tmp bus.fbd
+	python3 ../../../../main.py -p packages.tmp bus.fbd
 	sed '/Path/d' packages.tmp > packages.tmp.sed
 	sed '/Path/d' packages.golden > packages.golden.sed
 	cmp packages.golden.sed packages.tmp.sed
 	rm packages.tmp
 	rm *.sed
-	cd ..
+	cd ../..
 	echo
-	done
+done
