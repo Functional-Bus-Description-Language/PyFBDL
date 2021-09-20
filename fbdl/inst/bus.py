b858075b4783091e87349ebdab89929eecafb206
Michał Kruszewski <mkru@protonmail.com>
2021-09-20 12:03:24 +0200

    Add checking conflicting order of groups in statuses.

--- fbdl/inst/bus.py
+++ fbdl/inst/bus.py
@@ -2,6 +2,7 @@ import logging as log
 from pprint import pformat, pprint
 
 from . import args
+from .check import check_property, check_property_conflict, check_groups
 from .fill import set_bus_width, fill_missing_properties
 from .utils import get_file_path
 from ..validation import ValidElements
@@ -51,104 +52,6 @@ def resolve_to_base_type(symbol):
 
     type_chain.append(symbol)
     return type_chain
-
-
-def check_property(name, prop, elem_type):
-    """Check property value type, value and conflicting properties within element."""
-
-    def file_line_msg():
-        return f"File '{get_file_path(elem_type)}', line {prop['Line Number']}."
-
-    def wrong_type_msg(excepted_type):
-        return (
-            f"The '{name}' property must be of type '{excepted_type}'.\n"
-            + f"Current type '{type_.__name__}'.\n"
-            + file_line_msg()
-        )
-
-    def negative_value_msg():
-        return (
-            f"Negative value ({prop['Value'].value}) of the '{name}' property.\n"
-            + file_line_msg()
-        )
-
-    value = prop['Value'].value
-    type_ = type(value)
-    if name in ['atomic', 'once']:
-        if type_ != bool:
-            raise Exception(wrong_type_msg('bool'))
-    elif name == 'doc':
-        if type_ != str:
-            raise Exception(wrong_type_msg('str'))
-    elif name == 'groups':
-        if type_ != list:
-            raise Exception(wrong_type_msg('list'))
-        for group in value:
-            if type(group) != str:
-                raise Exception(
-                    "All values in the 'groups' property value list must be of type 'str'.\n"
-                    f"'{group}' is of type '{type(group).__name__}'.\n"
-                    + file_line_msg()
-                )
-    elif name == 'masters':
-        if type_ != int:
-            raise Exception(wrong_type_msg('int'))
-        if value < 1:
-            raise Exception(
-                f"Value of the 'masters' property must be positive.\n"
-                + f"Current value {value}.\n"
-                + file_line_msg()
-            )
-    elif name == 'range':
-        if type_ != list:
-            raise Exception(wrong_type_msg('list'))
-        elif len(value) != 2:
-            raise Exception(
-                "Length of the 'range' property value list must equal two.\n"
-                + f"Current length {len(value)}.\n"
-                + file_line_msg()
-            )
-        elif type(value[0]) != int or type(value[1]) != int:
-            raise Exception(
-                "Both values in the 'range' property value list must be of type 'int'.\n"
-                f"Current types '{type(value[0]).__name__}' and '{type(value[1]).__name__}'.\n"
-                + file_line_msg()
-            )
-        elif value[0] >= value[1]:
-            raise Exception(
-                "Second value in the 'range' property value list must be greater than the first one.\n"
-                + f"First value {value[0]}, second value {value[1]}.\n"
-                + file_line_msg()
-            )
-    elif name == 'width':
-        if type_ != int:
-            raise Exception(wrong_type_msg('int'))
-        if value < 0:
-            raise Exception(negative_value_msg())
-        if 'range' in elem_type['Properties']:
-            raise Exception(
-                "Can not set the 'width' property, because the 'range' property is "
-                + f"already set in line {elem_type['Properties']['range']['Line Number']}.\n"
-                + file_line_msg()
-            )
-
-
-def check_property_conflict(name, prop, elem_type, inst):
-    """Check properties conflict between types."""
-
-    def conflict_msg(first, second):
-        return (
-            f"Can not set the '{first}' property, because the '{second}' "
-            + "property is already set in one of the ancestor types.\n"
-            + f"File '{get_file_path(elem_type)}', line {prop['Line Number']}."
-        )
-
-    if name == 'range':
-        if 'width' in inst['Properties']:
-            raise Exception(conflict_msg('range', 'width'))
-    elif name == 'width':
-        if 'range' in inst['Properties']:
-            raise Exception(conflict_msg('width', 'range'))
 
 
 def instantiate_type(type, from_type, resolved_arguments):
@@ -264,5 +167,14 @@ def instantiate_element(element):
     log.debug(f"Instantiating element '{element['Name']}'.")
     type_chain = resolve_to_base_type(element)
     instance = instantiate_type_chain(type_chain)
+
+    if instance['Base Type'] in ['bus', 'block']:
+        conflicting_type, conflict = check_groups(instance)
+        if conflict:
+            raise Exception(
+                f"Conflicting order of groups in elements of type '{conflicting_type}' in element '{element['Name']}'.\n"
+                + conflict + '\n'
+                f"File '{get_file_path(type_chain[-1])}', line {type_chain[-1]['Line Number']}."
+            )
 
     return instance
